a1ca8a58f43ec3f04618eb18b82393dd069d03ed
luaos-bepo <2302115898@qq.com>
2020-06-17 22:35:33 +0800

    第二次上传

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,21 +5,50 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    imgSrc : '',
+    indicatorDots: true,  //开启面板控制点
+    vertical: false,     //false 表示不开启纵向移动，既开启横向移动
+    autoplay: true,    //是否开启自动切换
+    interval: 2000,  //切换间隔2s
+    duration: 100,    //滑动的时长
+    circular: true,
+    loading: true
   },
-
+ 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
+    var _this = this
+    wx.request({
+      url: 'http://192.168.1.103:3000/pic',
+      
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      method : "GET",
+      success (res) {
+        console.log(res.data.message[0].src)
+        _this.setResToData(res.data.message[0].src)
+      }
+    })
+    },
+    setResToData: function(data){
+      this.setData({imgSrc : data})
+    },
+ 
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.setData({loading : false})
   },
 
   /**
